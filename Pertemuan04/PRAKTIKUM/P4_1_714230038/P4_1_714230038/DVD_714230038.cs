522da1af73cbc7bfb39dbbdd06ab280b4efb14ee
Adul <rereeaa56@gmail.com>
2024-11-04 22:52:46 +0700

    Commit Praktikum

---
+++ Pertemuan04/PRAKTIKUM/P4_1_714230038/P4_1_714230038/DVD_714230038.cs
@@ -0,0 +1,33 @@
+﻿using P4_1_714230038;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714230038
+{
+    public class DVD_714230038 : Product_714230038
+    {
+        protected string duration;
+
+        public DVD_714230038(string title, string duration)
+        {
+            MyType = "DVD";
+            MyTitle = title;
+            this.duration = duration;
+            Console.WriteLine("ini dari class DVD");
+        }
+        public string Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+
+        //implementasi metode abstrak
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Product is a {0} called \"{1}\" and has {2} minutes duration", MyType, MyTitle, Duration);
+        }
+    }
+}
---
+++ Pertemuan04/PRAKTIKUM/P4_1_714230038/P4_1_714230038/ProductTest_NPM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714230038
+{
+    class ProductTest_714230038
+    {
+        static void Main(string[] args)
+        {
+            Book_714230038 product1 = new Book_714230038("Book", "c# object oriented solution", "300");
+
+            product1.DisplayInfo();
+        }
+    }
+}
