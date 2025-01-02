1fb998895cf3407799153ca4bd1516f7073da38d
rrq-dev <raihanhermawan85@gmail.com>
2025-01-02 15:10:14 +0700

    Praktikum 9-10

---
+++ Pertemuan09/Praktikum/P9_714230038/P9_714230038/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714230038
+{
+    public partial class Form1 : Form
+    {
+
+        Koneksi koneksi = new Koneksi();
+
+        public void Tampil()
+        {
+            //query db get MGS
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+
+            //mengubah nama kolom tabel
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[0].HeaderText = "Nama";
+            DataMahasiswa.Columns[0].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[0].HeaderText = "Alamat";
+            DataMahasiswa.Columns[0].HeaderText = "Email";
+            DataMahasiswa.Columns[0].HeaderText = "No HP";
+        }
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tampil();
+        }
+    }
+}
